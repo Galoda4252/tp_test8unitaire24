d14d73dedccf3250595593cf80c154bc808ca86b
Galoda4252 <saphiraroger08@gmail.com>
2024-04-02 14:14:04 +0200

    test add simple bag

--- tp_testunitaire/src/test/TestMixed.java
+++ tp_testunitaire/src/test/TestMixed.java
@@ -16,4 +16,12 @@ public class TestMixed {
         MoneyBag expected = new MoneyBag(bag);
         assertEquals(expected, f12CHF.add(f7USD));
     }
+    @Test
+    public void testSimpleBagAdd() {
+        Money f5CHF = new Money(5, "CHF");
+        Money f7USD = new Money(7, "USD");
+        Money bag[] = { f5CHF, f7USD };
+        MoneyBag expected = new MoneyBag(bag);
+        assertEquals(expected, f5CHF.add(f7USD));
+    }
 }
